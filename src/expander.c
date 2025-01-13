6ec750b6ecc38cb22d35107ee89e6be8757023ad
Anthony Goldberg <agoldber@mi-r5-p1.s19.be>
2025-01-13 17:28:09 +0100

    ajout de message de debug. l'expender fonctionnait en vif pendant un moment mais j'ai modifie un truc et tout a explose...

--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: agoldber <agoldber@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:42:57 by agoldber          #+#    #+#             */
-/*   Updated: 2025/01/13 15:36:55 by agoldber         ###   ########.fr       */
+/*   Updated: 2025/01/13 17:25:19 by agoldber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,32 @@ size_t	get_final_len(char *content, char *env)
 	size_t	len_content;
 	size_t	len_env;
 
+	printf("\n-------------GET_FINAL_LEN-------------\n");
 	if (!env || !*env)
-		len_env = 1;
+	{
+		printf("pas d'env donc len_env = 0\n");
+		len_env = 0;
+	}
 	else
+	{
 		len_env = ft_strlen(env);
+		printf("env donc len_env = %ld\n", len_env);
+	}
+	sleep(1);
 	if (!content)
+	{
+		printf("pas de content donc len_content = 0\n");
 		len_content = 0;
+	}
 	else
+	{
 		len_content = ft_strlen(content);
+		printf("content donc len_content = %ld\n", len_content);
+	}
+	sleep(1);
+	printf("res final : %ld\n", len_env + len_content);
+	printf("----------------------------------------\n");
+	sleep(1);
 	return (len_env + len_content);
 }
 
@@ -36,24 +54,41 @@ char	*get_extand_content(char *content)
 
 	i = 0;
 	len = 0;
+	printf("\n-------------EXTAND CONTENT-------------\n");
+	printf("premiere partie\n");
 	while (content[i])
 	{
+		printf("%c", content[i]);
 		if (content[i] == ' ' || content[i] == '|' || content[i] == '<' || content[i] == '>')
 			break ;
 		i++;
 	}
+	printf("\n");
+	sleep(1);
+	printf("deuxieme partie\n");
 	while (content[i])
 	{
+		printf("%c", content[i]);
 		i++;
 		len++;
 	}
+	printf("\n");
+	sleep(1);
 	res = malloc(len + 1);
+	printf("malloc(len = [%ld])\ni = [%d]\n", len, i);
 	if (!res)
 		return (NULL);
 	i -= len;
 	len = 0;
+	printf("content[i] : %c\n", content[i]);
+	printf("content[i + 1] : %c\n", content[i + 1]);
+	printf("on copie la deuxieme partie dans res\n");
+	sleep(1);
 	while (content[i])
 		res[len++] = content[i++];
+	res[len] = '\0';
+	printf("res : %s\n", res);
+	printf("-----------------------------------------\n\n");
 	return (res);
 }
 
@@ -62,26 +97,38 @@ void	change_content(char *content, char *env)
 	char	*temp;
 	size_t	len;
 
+	printf("-------------CHANGE CONTENT-------------\n");
 	len = 0;
 	while (*env != '=')
 		env++;
 	env++;
+	printf("on avance pour arriver apres le '=' : %s\n", env);
+	sleep(1);
 	temp = get_extand_content(content);
 	free(content);
 	if (!temp)
 		return ;
+	printf("on sort de get_extand_content...\ntemp = %s\n", temp);
 	len = get_final_len(temp, env) + 1;
-	content = malloc(len);
+	printf("on sort de get_final_len...\n");
+	content = calloc(len, sizeof(char));
 	if (!content)
 	{
 		free(temp);
 		return ;
 	}
-	if (!env || !*env)
-		ft_strlcpy(content, " ", len);
-	else
+	if (!env && !temp)
+	{
+		free(content);
+		content = ft_strdup(" ");
+		return ;
+	}
+	printf("env : %s\n", env);
+	if (env)
 		ft_strlcpy(content, env, len);
+	printf("temp : %s\n", temp);
 	ft_strlcat(content, temp, len);
+	printf("len : %ld\n", len);
 	return ;
 }
 
@@ -130,6 +177,7 @@ void	function_to_expand(char *content, char **env)
 	{
 		printf("le premier char est [$]\n");
 		len = get_expander_len(content) - 1;
+		printf("len ; %ld\n", len);
 		printf("len = [%ld]\n", len);
 		while (env[i])
 		{
