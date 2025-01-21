0d02c24c181239c52d6120873697ba15e7ee1cd1
Anthony Goldberg <agoldber@mi-r3-p6.s19.be>
2025-01-21 17:44:34 +0100

    je test des trucs

--- src/check_token.c
+++ src/check_token.c
@@ -6,7 +6,7 @@
 /*   By: agoldber <agoldber@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:10:55 by agoldber          #+#    #+#             */
-/*   Updated: 2025/01/20 17:32:18 by agoldber         ###   ########.fr       */
+/*   Updated: 2025/01/21 17:12:03 by agoldber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,73 @@ void	err_trunc_handler(t_token *current)
 {
 	current = current->next;
 	if (current->type == PIPE)
+	{
+		if (current->next && current->next->type == PIPE)
+		{
+			current = current->next;
+			if (current->next && current->next->type == PIPE)
+			{
+				print_syntax_error("||");
+			}
+			else
+			{
+				print_syntax_error("|");
+			}
+		}
+		else
+		{
+			if (current->next && current->next->type == R_TRUNC)
+			{
+				print_syntax_error(">");
+			}
+			else
+			{
+				print_syntax_error("newline");
+			}
+		}
+	}
 }
 
 void	error_handler(t_token *current)
 {
-	if (current->type == R_INPUT)
-	{
-		err_input_handler();
-	}
-	else if (current->type == R_TRUNC)
+	// if (current->type == R_INPUT)
+	// {
+	// 	err_input_handler();
+	// }
+	if (current->type == R_TRUNC)
 	{
 		err_trunc_handler(current);
 	}
-	else if (current->type == R_HEREDOC)
-	{
-		err_heredoc_handler();
-	}
-	else if (current->type == R_APPEND)
-	{
-		err_append_handler();
-	}
+	// else if (current->type == R_HEREDOC)
+	// {
+	// 	err_heredoc_handler();
+	// }
+	// else if (current->type == R_APPEND)
+	// {
+		// err_append_handler();
+	// }
+}
+
+void	pipe_handler(t_token **token)
+{
+	char	*inpt;
+
+	inpt = readline("pipe > ");
+	(void)token;
+	free(inpt);
+}
+
+void	solo_handler(t_token *current)
+{
+	if (current->type != PIPE)
+		print_syntax_error("newline");
+	else
+		print_syntax_error("|");
+}
+
+void	heredoc(void)
+{
+	printf("heredoc\n");
 }
 
 int	check_token(t_token **token)
@@ -48,30 +95,42 @@ int	check_token(t_token **token)
 	t_token	*current;
 
 	current = *token;
+	if (!current->next && current->type != WORD)
+		return (solo_handler(current), 0);
 	while (current)
 	{
+		if (current->type == R_HEREDOC)
+			heredoc();
 		if (current->next)
 		{
-			if (current->type != R_INPUT && current->type != R_TRUNC)
+			if (current->type != R_INPUT && current->next->type != R_TRUNC)
 			{
 				if (current->type != WORD && current->next->type != WORD)
 				{
 					return (error_handler(current), 0);
 				}
-				else if (current->type != WORD && current->type != PIPE && !current->next)
-				{
-					return (error_end_handle(current), 0);
-				}
-				else if (current->type == R_HEREDOC)
-				{
-					create_heredoc(current);
-				}
-				else if (current->type == PIPE && !current->next)
-				{
-					return (end_pipe_handle(token, current));
-				}
+				// else if (current->type != WORD && current->type != PIPE && !current->next)
+				// {
+				// 	return (error_end_handle(current), 0);
+				// }
+				// else if (current->type == R_HEREDOC)
+				// {
+				// 	create_heredoc(current);
+				// }
+				// else if (current->type == PIPE && !current->next)
+				// {
+				// 	return (end_pipe_handle(token, current));
+				// }
 			}
 			current = current->next;
+		}
+		else if (current->type != PIPE)
+		{
+			return (solo_handler(current), 0);
+		}
+		else if (current->type == PIPE && !current->next)
+		{
+			pipe_handler(token);
 		}
 		else
 			break ;
