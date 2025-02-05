57ab0fd0251fd5c2e4cdb60a42efbbb9aca0b7f5
Anthony Goldberg <agoldber@mi-r3-p5.s19.be>
2025-02-05 16:59:06 +0100

    debut de heredoc dans un pipe

--- src/check_token/heredoc.c
+++ src/check_token/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: agoldber <agoldber@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 15:47:05 by agoldber          #+#    #+#             */
-/*   Updated: 2025/01/24 18:45:31 by agoldber         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:54:10 by agoldber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,31 @@
 void	heredoc(char *delimiter, int expand)
 {
 	char	*inpt;
+	int		pipe_buf[2];
 
 	inpt = NULL;
+	if (pipe(pipe_buf) == -1)
+		return (print_error_message(1, "pipe", "Cannot allocate memory"));
 	while (1)
 	{
 		inpt = readline("heredoc > ");
+		if (!inpt)
+		{
+			close(pipe_buf[1]);
+			close(pipe_buf[0]);
+			return (print_error_message(1, "pipe", "Cannot allocate memory"));
+		}
 		if (!(ft_strncmp(delimiter, inpt, ft_strlen(delimiter) + 1)))
 			break ;
-		if (expand)
-		{
+		// if (expand)
+		// {
 			
-		}
+		// }
+		ft_putstr_fd(inpt, pipe_buf[1]);
+		ft_putstr_fd("\n", pipe_buf[1]);
 		free(inpt);
 	}
 	printf("inpt : %s\n", inpt);
+	close(pipe_buf[1]);
 	free(inpt);
 }
